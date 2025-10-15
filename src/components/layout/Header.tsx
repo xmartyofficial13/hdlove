8f3ce73af0df3d2442c79b2361d206b40555c804
DHANJEE RIDER <dhanjeekumar775145@gmail.com>
2025-10-15 03:25:18 +0000

    Remove left right padding  (_for element div_)

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -11,7 +11,7 @@ export async function Header() {
 
   return (
     <header className="sticky top-0 z-50 w-full border-b border-border/40 bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
-      <div className="container flex h-16 max-w-screen-2xl items-center">
+      <div className="flex h-16 max-w-screen-2xl items-center px-4">
         <div className="mr-4 flex">
             <Logo />
         </div>
