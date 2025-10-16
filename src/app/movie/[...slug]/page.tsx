0df29b10cd75dc4e8feed60a2ff07db8a3453aac
DHANJEE RIDER <dhanjeekumar775145@gmail.com>
2025-10-16 10:30:27 +0000

    Video Not Found ! watch now imdb wale me a rha hai site pe koi protectin

--- src/app/movie/[...slug]/page.tsx
+++ src/app/movie/[...slug]/page.tsx
@@ -181,6 +181,7 @@ export default async function MoviePage({ params }: { params: { slug: string[] }
                         frameBorder="0"
                         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
                         allowFullScreen
+                        referrerPolicy="unsafe-url"
                         className="h-full w-full"
                     ></iframe>
                 </div>
