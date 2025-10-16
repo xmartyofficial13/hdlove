e82a6d43d44a9758ac289cad1c96778e0c511296
DHANJEE RIDER <dhanjeekumar775145@gmail.com>
2025-10-16 11:27:09 +0000

    Payer page 404 https://hdlove4u.vercel.app/player?url=https%3A%2F%2Fhdst

--- src/app/api/scrape/route.ts
+++ src/app/api/scrape/route.ts
@@ -13,7 +13,7 @@ export async function GET(request: Request) {
     const response = await fetch(url, {
         headers: {
             'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36',
-            'Referer': 'https://hdhub4u.cologne/' 
+            'Referer': new URL(url).origin
         }
     });
 
