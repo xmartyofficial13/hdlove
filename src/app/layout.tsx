aed6374fb1aa6eb3271d56495cd616edc002f1d8
DHANJEE RIDER <dhanjeekumar775145@gmail.com>
2025-10-17 01:17:54 +0000

    Righ click aur inspect desable krdo

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -71,6 +71,27 @@ export default function RootLayout({
           <Toaster />
         </ThemeProvider>
         <script src='https://dktczn.github.io/Dk/cdn/autoupdate.js'/>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              document.addEventListener('contextmenu', event => event.preventDefault());
+              document.onkeydown = function (e) {
+                if (e.keyCode == 123) { // F12
+                    return false;
+                }
+                if (e.ctrlKey && e.shiftKey && e.keyCode == 'I'.charCodeAt(0)) { // Ctrl+Shift+I
+                    return false;
+                }
+                if (e.ctrlKey && e.shiftKey && e.keyCode == 'J'.charCodeAt(0)) { // Ctrl+Shift+J
+                    return false;
+                }
+                if (e.ctrlKey && e.keyCode == 'U'.charCodeAt(0)) { // Ctrl+U
+                    return false;
+                }
+              };
+            `,
+          }}
+        />
       </body>
     </html>
   );
